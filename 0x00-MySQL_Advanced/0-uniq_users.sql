84d37bc2fb45330a43e9e89bfea67a5ebc85dbe5
kingsleyabonyi <kinabonyi@gmail.com>
2023-02-08 23:08:00 +0100

    fiX up

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- id, email, name
 
 CREATE TABLE IF NOT EXISTS users (
-	id int NOT NULL AUTO INCREMENT PRIMARY KEY,
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255)
 )
