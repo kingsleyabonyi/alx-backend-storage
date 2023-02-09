5bbf8066590d9f6f5be0ef678f94090fd3300d34
kingsleyabonyi <kinabonyi@gmail.com>
2023-02-09 01:01:06 +0100

    script that creates table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- a script thst creates a table user with the attributs below
 -- id, email, name, country
 
-CREATE TABLE IF  NOT EXIST users(
+CREATE TABLE IF  NOT EXISTS users(
 	id int NOT AUTO_INCREMENT PRIMARY KEY,
 	email varchar(255) NOT NULL UNIQUE,
 	name varchar(255),
